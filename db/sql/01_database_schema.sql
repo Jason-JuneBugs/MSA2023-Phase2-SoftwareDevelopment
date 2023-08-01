3f369c587466d009382a13d07ca13d797e9ca524
Jason Zhong <jasonzhong@developers.institute.co.nz>
2023-08-02 00:21:53 +1200

    create database table and insert data

--- db/sql/01_database_schema.sql
+++ db/sql/01_database_schema.sql
@@ -2,13 +2,21 @@
  ------------ Create Tables -------------
 
 CREATE TABLE planets(id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-                                                      keplerName varchar(255) NOT NULL);
+                                                      keplerName VARCHAR(255) NOT NULL);
 
+-- CREATE TABLE launches(id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+--                                                        flightNumber INTEGER NOT NULL,
+--                                                                             launchDate DATE NOT NULL,
+--                                                                                             mission VARCHAR NOT NULL,
+--                                                                                                             rocket VARCHAR NOT NULL,
+--                                                                                                                            target VARCHAR,customers VARCHAR ARRAY,upcoming BOOLEAN NOT NULL,
+--                                                                                                                                                                                    success BOOLEAN NOT NULL)
 
-CREATE TABLE launches(id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-                                                       flightNumber INTEGER NOT NULL,
-                                                                            launchDate DATE NOT NULL,
-                                                                                            mission VARCHAR NOT NULL,
-                                                                                                            rocket VARCHAR NOT NULL,
-                                                                                                                           target VARCHAR,customers VARCHAR ARRAY,upcoming BOOLEAN NOT NULL,
-                                                                                                                                                                                   success BOOLEAN NOT NULL) -- COMMIT
+CREATE TABLE launches (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+                                                        flightNumber INTEGER NOT NULL,
+                                                                             launchDate TIMESTAMP WITH TIME ZONE NOT NULL,
+                                                                                                                 mission VARCHAR NOT NULL,
+                                                                                                                                 rocket VARCHAR NOT NULL,
+                                                                                                                                                target VARCHAR, customers VARCHAR[], upcoming BOOLEAN , success BOOLEAN);
+
+-- COMMIT
