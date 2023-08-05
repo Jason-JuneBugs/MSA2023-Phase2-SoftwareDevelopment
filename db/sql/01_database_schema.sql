abc9d3df44739a6870fafa1a3425896f094aa756
Jason Zhong <jasonzhong@developers.institute.co.nz>
2023-08-05 19:01:33 +1200

    update DB schema

--- db/sql/01_database_schema.sql
+++ db/sql/01_database_schema.sql
@@ -8,10 +8,10 @@ CREATE TABLE planets(id integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 
 
 CREATE TABLE launches (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-                                                        flightNumber INTEGER , launchDate TIMESTAMP WITH TIME ZONE NOT NULL,
-                                                                                                                   mission VARCHAR NOT NULL,
-                                                                                                                                   rocket VARCHAR NOT NULL,
-                                                                                                                                                  target_id INTEGER, customers VARCHAR[], upcoming BOOLEAN , success BOOLEAN DEFAULT NULL);
+                                                        flightNumber INTEGER, launchDate TIMESTAMP WITH TIME ZONE NOT NULL,
+                                                                                                                  mission VARCHAR NOT NULL,
+                                                                                                                                  rocket VARCHAR NOT NULL,
+                                                                                                                                                 target_id INTEGER, customers VARCHAR[], upcoming BOOLEAN , success BOOLEAN DEFAULT NULL);
 
 
 COMMIT
